4857244c3ed1fc0a047b6572e8f0aad9ec2d0842
Valdimir Ladynev <v.ladynev@gmail.com>
2017-03-15 21:03:24 +0300

    added examples for Admin API

--- adminClient.js
+++ adminClient.js
@@ -1,6 +1,8 @@
 `use strict`;
 
 let adminClient = require('keycloak-admin-client');
+const getToken = require('keycloak-request-token');
+const request = require('request');
 
 let settings = {
     baseUrl: 'http://127.0.0.1:8080/auth',
@@ -39,9 +41,90 @@ exports.createTestUser = function (callback) {
         .then((client) => {
             client.users.create(exampleSettings.realmName, {
                 username: 'test_user',
+                firstName: 'user first name',
                 enabled: true,
                 credentials: [{type: 'password', value: 'password'}]
             }).then(callback, error(callback));
+        })
+        .catch(error(callback));
+};
+
+exports.updateTestUser = function (callback) {
+    adminClient(settings)
+        .then((client) => {
+
+            exports.findTestUser(users => {
+                    let user = users && users[0];
+                    if (user) {
+                        user.firstName = 'user first name updated';
+                        client.users.update(exampleSettings.realmName, user)
+                            .then(callback.bind(null, "updated"), error(callback));
+                    } else {
+                        callback("not found");
+                    }
+                }, error(callback)
+            );
+
+        })
+        .catch(error(callback));
+};
+
+exports.setTestUserCustomerId = function (callback) {
+    adminClient(settings)
+        .then((client) => {
+
+            exports.findTestUser(users => {
+                    let user = users && users[0];
+                    if (user) {
+                        user.attributes = user.attributes || {};
+                        user.attributes.customerId = 123;
+                        client.users.update(exampleSettings.realmName, user)
+                            .then(callback.bind(null, "updated"), error(callback));
+                    } else {
+                        callback("not found");
+                    }
+                }, error(callback)
+            );
+
+        })
+        .catch(error(callback));
+};
+
+exports.removeTestUserCustomerId = function (callback) {
+    adminClient(settings)
+        .then((client) => {
+
+            exports.findTestUser(users => {
+                    let user = users && users[0];
+                    if (user) {
+                        user.attributes = user.attributes || {};
+                        user.attributes.customerId = undefined;
+                        client.users.update(exampleSettings.realmName, user)
+                            .then(callback.bind(null, "updated"), error(callback));
+                    } else {
+                        callback("not found");
+                    }
+                }, error(callback)
+            );
+
+        })
+        .catch(error(callback));
+};
+
+exports.getTestUser = function (callback) {
+    adminClient(settings)
+        .then((client) => {
+
+            exports.findTestUser(users => {
+                    if (users && users[0] && users[0].id) {
+                        client.users.find(exampleSettings.realmName, {userId: users[0].id})
+                            .then(callback, error(callback));
+                    } else {
+                        callback("not found");
+                    }
+                }, error(callback)
+            );
+
         })
         .catch(error(callback));
 };
@@ -78,6 +161,51 @@ exports.deleteUserById = function (userId, callback) {
             client.users.remove(exampleSettings.realmName, userId).then(callback, error(callback));
         }).catch(error(callback));
 };
+
+exports.createRole = function (callback) {
+    authenticate(token => {
+        keycloakRequest('POST', `/admin/realms/${exampleSettings.realmName}/roles`, token, {name: 'TEST_ROLE'})
+            .then(callback.bind(null, 'created'), error(callback))
+            .catch(error(callback));
+    });
+};
+
+function authenticate(callback) {
+    return getToken(settings.baseUrl, settings)
+        .then(callback);
+}
+
+function keycloakRequest(method, url, accessToken, jsonBody) {
+    return new Promise((resolve, reject) => {
+        let req = method == 'POST' || method == 'PUT' ? {
+            url: settings.baseUrl + url,
+            auth: {
+                bearer: accessToken
+            },
+            body: jsonBody,
+            method: method,
+            json: true
+        } : {
+            url: settings.baseUrl + url,
+            auth: {
+                bearer: accessToken
+            },
+            method: method
+        };
+
+        request(req, (err, resp, body) => {
+            if (err) {
+                return reject(err);
+            }
+
+            if (resp.statusCode !== 200 && resp.statusCode !== 201 && resp.statusCode !== 204) {
+                return reject(body);
+            }
+
+            return resolve(body);
+        });
+    });
+}
 
 function error(callback) {
     return function (errorObject) {
